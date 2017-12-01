4028eb78c64eb1ce477d8575f4fd176e5add1652
Fabio Santos <fabio.s@webinterpret.com>
2017-12-01 16:59:09 +0100

    replace every with some in except option

--- src/core.js
+++ src/core.js
@@ -1,5 +1,5 @@
 export const checkVisibility = (roles, allowed, except) => {
-  if (except.length > 0 && except.every(elem => roles.indexOf(elem) > -1)) {
+  if (except.length > 0 && except.some(elem => roles.indexOf(elem) > -1)) {
     return false
   }
   if (allowed.length > 0) {
